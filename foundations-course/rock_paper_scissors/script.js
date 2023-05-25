4f01700a1efdfabfbe6d4011b4075d2915ce3597
FDMetz <metzfederico@hotmail.com>
2023-05-25 11:17:50 -0300

    docs: create playerChoice and getPlayerChoice

--- foundations-course/rock_paper_scissors/script.js
+++ foundations-course/rock_paper_scissors/script.js
@@ -1,7 +1,16 @@
-const choice = ["Rock","Paper","Scissors"];
+const choice = ["rock","paper","scissors"];
+let playerChoice = null;
 
 function getComputerChoice(){
     return choice[Math.floor(Math.random()*3)];
 }
 
-//console.log(getComputerChoice());
+function getPlayerChoice(){
+    while ( playerChoice==null ){
+        playerChoice = prompt("Choice: rock, paper or scissors.");
+        if (playerChoice.toLowerCase()!=choice[0] && playerChoice.toLowerCase()!=choice[1] && playerChoice.toLowerCase()!=choice[0]){
+            playerChoice=null;
+            alert("invalid option");
+        }
+    }
+}
