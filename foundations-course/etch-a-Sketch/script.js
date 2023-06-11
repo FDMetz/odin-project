5803ec5c6d8453f1f5f7b65b5a1bb7598b0c9b1e
FDMetz <metzfederico@hotmail.com>
2023-06-11 15:11:43 -0300

    Create the code to make the code and add style.
    Create grid-container div and the functions: createRows, appendRows,
    createColumns and appendColumns. Besides, add style to the grid
    elements.

---
+++ foundations-course/etch-a-Sketch/script.js
@@ -0,0 +1,39 @@
+const gridContainer = document.querySelector(".grid-container");
+const rows = [];
+const columns = [];
+
+function createRows (rowNum){
+    for(let n=0; n<rowNum; n++){
+        rows[n] = document.createElement("div");
+        rows[n].setAttribute("class","row");
+    }
+}
+
+function appendRows (rowNum){
+    for(let n=0; n<rowNum; n++){
+        gridContainer.appendChild(rows[n]);
+    }
+}
+
+function createColumns (rowNum,colmNum){
+    for(let r=0; r<rowNum; r++){
+        columns.push([]);
+        for(let c=0; c<colmNum; c++){
+            columns[r][c] = document.createElement ("div");
+            columns[r][c].setAttribute ("class","column");
+        }
+    }
+}
+
+function appendColumns (rowNum,colmNum){
+    for(let r=0; r<rowNum; r++){
+        for(let c=0; c<colmNum; c++){
+            rows[r].appendChild(columns[r][c]);
+        }
+    }
+}
+
+createRows(16);
+appendRows(16);
+createColumns(16,16);
+appendColumns(16,16);
