9e085fb43e81d960fcad610f33bceee152a470b0
FDMetz <metzfederico@hotmail.com>
2023-06-17 15:48:57 -0300

    refactor: changes the main code
    Replace functions that manage columns and rows by the
    functions createCell and appendCell.
    
    Define variable "size" to set the number of cells.

--- foundations-course/etch-a-Sketch/script.js
+++ foundations-course/etch-a-Sketch/script.js
@@ -1,48 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
-const rows = [];
-const columns = [];
+let size = 16;
+const button = document.querySelector("button");
 
-function createRows (rowNum){
-    for(let n=0; n<rowNum; n++){
-        rows[n] = document.createElement("div");
-        rows[n].setAttribute("class","row");
+function createGrid (cellNum){
+    for(let i=0; i<size; i++){
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.textContent = i;
+        appendCell(cell);
     }
 }
 
-function appendRows (rowNum){
-    for(let n=0; n<rowNum; n++){
-        gridContainer.appendChild(rows[n]);
-    }
-}
-
-function createColumns (rowNum,colmNum){
-    for(let r=0; r<rowNum; r++){
-        columns.push([]);
-        for(let c=0; c<colmNum; c++){
-            columns[r][c] = document.createElement ("div");
-            columns[r][c].setAttribute ("class","column");
-        }
-    }
-}
-
-function appendColumns (rowNum,colmNum){
-    for(let r=0; r<rowNum; r++){
-        for(let c=0; c<colmNum; c++){
-            rows[r].appendChild(columns[r][c]);
-        }
-    }
+function appendCell(child){
+    gridContainer.appendChild(child);
 }
 
 function changeClass(e){
     if(e.target.className == "column"){
         e.target.setAttribute("class","hover");
-    }
-        
+    }     
 }
 
-createRows(16);
-appendRows(16);
-createColumns(16,16);
-appendColumns(16,16);
-
+createGrid(size);
 document.addEventListener("mouseover",changeClass);
