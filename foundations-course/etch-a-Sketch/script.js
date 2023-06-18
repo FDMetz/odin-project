f2aa695134ff2799fe54e7c5290092c0181d9fc7
FDMetz <metzfederico@hotmail.com>
2023-06-18 18:32:58 -0300

    Add variables and functions to resize the grid
    Create removeGrid and resize, variable "columns" is no longer a const.

--- foundations-course/etch-a-Sketch/script.js
+++ foundations-course/etch-a-Sketch/script.js
@@ -1,17 +1,18 @@
-const gridContainer = document.querySelector("div");
+const gridContainer = document.querySelector(".grid-container");
 let side = 16;
+const button = document.querySelector("button");
 
-function createElement(elementName,className) {
+function createElement(elementName,className){
     const newElemnt = document.createElement(elementName);
     newElemnt.className = className;
     return newElemnt;
 }
 
-function appendElement(parent,child) {
+function appendElement(parent,child){
     parent.appendChild(child);
 }
 
-function createGrid(cellNum) {
+function createGrid(cellNum){
     for( let r=0; r<cellNum; r++){
         const row = createElement("div","row");
         appendElement (gridContainer,row);
@@ -22,10 +23,6 @@ function createGrid(cellNum) {
     }
 }
 
-createGrid(side);
-const columns = document.querySelectorAll(".column");
-columns.forEach(addEvent);
-
 function transform(column){
     column.target.classList.add("hover");
 }
@@ -33,3 +30,25 @@ function transform(column){
 function addEvent(column){
     column.addEventListener("mouseover",transform);
 }
+
+function resize(){
+    do{
+        side = prompt("Enter side of new grid (max 100)",16);
+    }while( side>100 || side<1)
+    removeGrid();
+    createGrid(side);
+    columns = document.querySelectorAll(".column");
+    columns.forEach(addEvent);
+}
+
+function removeGrid(){
+    let child = document.getElementsByClassName("row");
+    while( child.item(0) ){
+        gridContainer.removeChild(child.item(0));
+    }
+}
+
+createGrid(side);
+button.addEventListener("click",resize);
+let columns = document.querySelectorAll(".column");
+columns.forEach(addEvent);
