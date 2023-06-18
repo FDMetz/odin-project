4b48e403dfe606842acf3840c3b4e947ecaa0524
FDMetz <metzfederico@hotmail.com>
2023-06-18 12:18:02 -0300

    Add code to change cells when hover over them.

--- foundations-course/etch-a-Sketch/script.js
+++ foundations-course/etch-a-Sketch/script.js
@@ -15,7 +15,21 @@ function createGrid(cellNum) {
     for( let r=0; r<cellNum; r++){
         const row = createElement("div","row");
         appendElement (gridContainer,row);
+        for( let c=0; c<cellNum; c++){
+            const col = createElement("div","column");
+            appendElement (row,col);
+        }
     }
 }
 
 createGrid(side);
+const columns = document.querySelectorAll(".column");
+columns.forEach(addEvent);
+
+function transform(column){
+    column.target.classList.add("hover");
+}
+
+function addEvent(column){
+    column.addEventListener("mouseover",transform);
+}
