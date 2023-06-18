64648c8090b0927b8042931f769a9dab9db15a8a
FDMetz <metzfederico@hotmail.com>
2023-06-18 09:47:20 -0300

    Make changes to the functions and add a new one
    Rename createRow to createElement and appendRow to appendElement,
    createElement now recieve two parameters. Also added createGrid.

--- foundations-course/etch-a-Sketch/script.js
+++ foundations-course/etch-a-Sketch/script.js
@@ -1,12 +1,21 @@
 const gridContainer = document.querySelector("div");
 let side = 16;
 
-function createRow() {
-    const row = document.createElement("div");
-    row.className = "row";
-    return row;
+function createElement(elementName,className) {
+    const newElemnt = document.createElement(elementName);
+    newElemnt.className = className;
+    return newElemnt;
 }
 
-function appendRow(parent,child) {
+function appendElement(parent,child) {
     parent.appendChild(child);
 }
+
+function createGrid(cellNum) {
+    for( let r=0; r<cellNum; r++){
+        const row = createElement("div","row");
+        appendElement (gridContainer,row);
+    }
+}
+
+createGrid(side);
